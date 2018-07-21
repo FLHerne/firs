c81ef99bad93b81a2747993123904cead5e44064
Andy <andy@teamrubber.com>
2018-07-21 07:53:48 +0100

    Change: drop aluminium chain from Extreme

--- src/industries/aluminium_plant.py
+++ src/industries/aluminium_plant.py
@@ -14,7 +14,8 @@ industry = IndustrySecondary(id='aluminium_plant',
                              intro_year=1900,
                              graphics_change_dates=[1942, 1980])
 
-industry.economy_variations['EXTREME'].enabled = True
+# unused
+#industry.economy_variations['EXTREME'].enabled = True
 
 industry.add_tile(id='aluminium_plant_tile_1',
                   animation_length=47,
